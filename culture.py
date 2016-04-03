c2ca564f48d8f51d35b1b39e379eaa65c75d8a58
Aleksander Alafuzoff <aleksander.alafuzoff@gmail.com>
2016-04-03 18:31:41 +0300

    Removed Culture object and replaced it with a ParticleSystem instance. Added food visualization (also ParticleSystem). Animations to be changed to the same implementation.

--- culture.py
+++ culture.py
@@ -5,33 +5,34 @@ from OpenGL import GL
 
 import boa_gfx
 from boa_gfx import Mesh, VBO, VAO
-from boa_gfx.gl_texture import TextureArray
+from boa_gfx.gl_texture import TextureArray, Texture
+from boa_gfx.gl_particle_system import ParticleSystem
 import ctypes
 
 import SharedArray as sa
 
 
-ALIVE = 0
-ID = 1
-BASE_POS_X = 2
-BASE_POS_Y = 3
-BASE_ROT = 4
-AGE = 5
-MAX_AGE = 6
-SPEED = 7
-AGGR = 8
-MOJO = 9
-FERT = 10
-INT_COOL = 11
+#ALIVE = 0
+#ID = 1
+#BASE_POS_X = 2
+#BASE_POS_Y = 3
+#BASE_ROT = 4
+#AGE = 5
+#MAX_AGE = 6
+#SPEED = 7
+#AGGR = 8
+#MOJO = 9
+#FERT = 10
+#INT_COOL = 11
 
 # ALIVE
 # ID
 # BASE_POS_X
 # BASE_POS_Y
 # BASE_ROT
-POS_X = 5
-POS_Y = 6
-ROT = 7
+#POS_X = 5
+#POS_Y = 6
+#ROT = 7
 
 square = np.array([[-1.0,  1.0, 0.0, 1.0],  # X, Y, S, T
                    [-1.0, -1.0, 0.0, 0.0],
@@ -65,51 +66,42 @@ CREATURE_DATA = [{'name': 'creature_position',
                   'divisor': 1}]
 
 
-class Culture(Mesh):
-    def __init__(self):
-        super().__init__()
+def creature_system():
+    static_data = square
+    static_data_attributes = VAO.TEXTURED_DATA_2D
 
-        self.creature_parts = sa.attach('creature_parts')
+    instance_data = sa.attach('creature_parts')
+    instance_data_attributes = CREATURE_DATA
 
-        self.shader = boa_gfx.gl_shader.shader_manager.get_shader('creature.shader')
-        self.shader.bind()
-        self.shader.t_loc = self.shader.uniform_location('t')
-        self.shader.image_loc = self.shader.uniform_location('image')
-        GL.glUniform1i(self.shader.image_loc, 0)
-        self.shader.unbind()
-        #print(type(self.shader))
+    shader = boa_gfx.gl_shader.shader_manager.get_shader('creature.shader')
+    texture = TextureArray.from_directory('./128x128/')
 
-        self.texture_array = TextureArray.from_directory('./128x128/')
-        print(self.texture_array.layers)
+    system = ParticleSystem(static_data, static_data_attributes, instance_data, instance_data_attributes, shader, texture)
+    return system
 
-        self.quad_vbo = VBO(square)
-        self.creature_vbo = VBO(self.creature_parts, usage=GL.GL_DYNAMIC_DRAW)
-        self.vao = VAO()
-        self.vao.add_vbo(self.quad_vbo, VAO.TEXTURED_DATA_2D)
-        self.vao.add_vbo(self.creature_vbo, CREATURE_DATA)
+FOOD_DATA = [{'name': 'food_position',
+              'index': 2,
+              'components': 4,
+              'gl_type': GL.GL_FLOAT,
+              'normalized': GL.GL_FALSE,
+              'stride': 4 * 4,
+              'offset': ctypes.c_void_p(0),
+              'divisor': 1}]
 
-        self.scene.drawable.append(self)
 
-    def update(self, dt):
-        self.creature_vbo.update_data(self.creature_parts, 0)
+def food_system():
+    static_data = square
+    static_data_attributes = VAO.TEXTURED_DATA_2D
 
-    def draw(self):
-        #print(self.shader.vp_matrix)
-        t = time.perf_counter()
-        self.shader.bind()
-        GL.glUniform1f(self.shader.t_loc, t)
-        self.shader.unbind()
-        self.shader.m_matrix = self.m_matrix
+    instance_data = sa.attach('food_gfx')
+    instance_data_attributes = FOOD_DATA
 
-        self.texture_array.bind()
-        self.shader.bind()
-        self.vao.bind()
+    shader = boa_gfx.gl_shader.shader_manager.get_shader('food.shader')
+    texture = Texture.from_file('./textures/food/food.png')
 
-        GL.glDrawArraysInstanced(GL.GL_TRIANGLE_STRIP, 0, 4, self.creature_parts.shape[0])
+    system = ParticleSystem(static_data, static_data_attributes, instance_data, instance_data_attributes, shader, texture)
+    return system
 
-        self.vao.unbind()
-        self.shader.unbind()
-        self.texture_array.unbind()
 
 
 ANIMATION_DATA = [{'name': 'animation_position',
